65edcc80c561db9d5eb9bc513df295f03f4ceb12
Noah Finnerman <nonothetoad@gmail.com>
2017-09-15 16:26:43 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@
    /*---------------------------------------------------- */
 	/* Final Countdown Settings
 	------------------------------------------------------ */
-	var finalDate = '2017/01/01';
+	var finalDate = '2017/12/13';
 
 	$('div#counter').countdown(finalDate)
    	.on('update.countdown', function(event) {
